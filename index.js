171a62a3eff3c4f0f2d688511dcfc26fc367ad40
Satoshi <sumomo.tomo@gmail.com>
2018-11-26 15:48:23 +0900

    ignore node modules and refactor index

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const clova = require('@line/clova-cek-sdk-nodejs');
-const line = require('@line/bot-sdk');
-const express = require('express');
-const axios = require('axios');
+const clova = require('@line/clova-cek-sdk-nodejs')
+const line = require('@line/bot-sdk')
+const express = require('express')
+const axios = require('axios')
 
 async function getRestaurant(){
-  const url = 'GNAVI_SEARCH_URL'
-  const res = await axios.get(url);
-  return res.data.rest.name_kana
+  const url = 'GNAVI_SEARCH_API_URL'
+  const response = await axios.get(url)
+  return {name: response.data.rest[0].name_kana, url: response.data.rest[0].url}
 }
 
-async function getRestaurantUrl(){
-    const url = 'GNAVI_SEARCH_URL'
-    const res = await axios.get(url);
-    return res.data.rest.url
-}
+function sendLine(restaurantUrl){
+  const userId = 'USER_ID'
+  const client = new line.Client({
+    channelAccessToken: 'ACCESS_TOKEN'
+  })
 
-// 応答の最後に追加するテンプレート
-const TEMPLATE_INQUIRY = '自慢を教えて';
+  const message = {
+    type: 'text',
+    text: `提案したお店だよ ${restaurantUrl}`
+  }
+
+  client.pushMessage(userId, message)
+}
 
 const clovaSkillHandler = clova.Client
   .configureSkill()
@@ -25,75 +30,51 @@ const clovaSkillHandler = clova.Client
     responseHelper.setSimpleSpeech({
       lang: 'ja',
       type: 'PlainText',
-      value: `はーい。「おだてるちゃん」だよ。${TEMPLATE_INQUIRY}`,
-    });
+      value: `はーい。おだてるちゃんだよ。自慢を教えて`,
+    })
   })
   // カスタムインテント or ビルトインインテント
   .onIntentRequest(async responseHelper => {
-    const intent = responseHelper.getIntentName();
-    const userId = 'USER_ID';
-    let output_speech;
-    let restName = await getRestaurant();
+    const intent = responseHelper.getIntentName()
+    let outputSpeech
     switch (intent) {
       // ユーザーのインプットが自慢だと判別された場合。
       case 'BoastIntent': {
         // 自慢（スロット）を取得
         const slots = responseHelper.getSlots()
-        if(slots.life != null) {
-          output_speech = {
+        if (slots.boast != null) {
+          const restaurant = await getRestaurant()
+          outputSpeech = {
             "type": "SpeechList",
             "values": [
               {
                 "type": "URL",
                 "lang": "",
-                "value": "SOUND_URL"
-              }
-            ]
-          }
-          responseHelper.setOutputSpeech(output_speech)
-        }else if(slots.work != null) {
-          const client = new line.Client({
-            channelAccessToken: 'ACCESS_TOKEN'
-          });
-
-          const restUrl = await getRestaurantUrl()
-          const message = {
-            type: 'text',
-            text: `提案したお店だよ ${restUrl}`
-          };
-
-          client.pushMessage(userId, message)
-
-          output_speech = {
-            "type": "SpeechList",
-            "values": [
-              {
-                "type": "URL",
-                "lang": "" ,
                 "value": "SOUND_URL"
               },
               {
                 "type": "PlainText",
                 "lang": "ja",
-                "value": `明日${restName}に行こうよ`
+                "value": `やったね。今度頑張ったご褒美として${restaurant.name}に行こうよ`
               }
             ]
           }
-          responseHelper.setOutputSpeech(output_speech)
+          sendLine(restaurant.url)
+          responseHelper.setOutputSpeech(outputSpeech)
         }else{
-          output_speech = {
+          outputSpeech = {
             "type": "SpeechList",
             "values": [
               {
-                "type": "URL",
+                "type": "PlainText",
                 "lang": "",
-                "value": "SOUND_URL"
+                "value": "すごいね。他にはない。もっと聞かせて。"
               }
             ]
           }
-          responseHelper.setOutputSpeech(output_speech)
+          responseHelper.setOutputSpeech(outputSpeech)
         }
-        break;
+        break
       }
     }
   })
@@ -106,17 +87,15 @@ const clovaSkillHandler = clova.Client
     }
     responseHelper.setSimpleSpeech(speech)
   })
-  .handle();
+  .handle()
 
-const app = new express();
-//TODO
-// リクエストの検証を行う場合。環境変数APPLICATION_ID(値はClova Developer Center上で入力したExtension ID)が必須
+const app = new express()
 const clovaMiddleware = clova.Middleware({
   applicationId: process.env.APPLICATION_ID
-});
-app.post('/clova', clovaMiddleware, clovaSkillHandler);
+})
+app.post('/clova', clovaMiddleware, clovaSkillHandler)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server running on ${port}`);
-});
+  console.log(`Server running on ${port}`)
+})
